ebe2464463a3149d5428668b33c0e449607e9da3
Puneet <puneetjnv289@gmail.com>
2025-02-06 11:56:50 +0530

    check for auth

--- server/src/middlewares/auth.middlewares.js
+++ server/src/middlewares/auth.middlewares.js
@@ -5,11 +5,13 @@ import { asyncHandler } from '../utils/asyncHandler.js';
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     console.log("Verifying JWT");
-
+    console.log("req.cookies: ", req.cookies);
+    console.log("req.header: ", req.header("Authorization"))
     // Retrieve token from cookies or Authorization header
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
+        console.log(token);
         throw new ApiError(405, "Authentication token missing");
     }
 
@@ -19,7 +21,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
 
         // Fetch the user details excluding sensitive fields
         const user = await User.findById(decodedToken._id).select("-password -refreshToken");
-
+        console.log("user is in auth middleware: ", user);
         if (!user) {
             throw new ApiError(403, "User not found or invalid token");
         }
